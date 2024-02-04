a6f9bf32facbbe53df1e156317ddf938276a40f2
worksofallwin <princeallwin199@gmail.com>
2024-02-04 11:16:28 +0530

    complete dashboard page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Navbar } from "./_components/navbar";
+import { Footer } from "./_components/footer";
 
 type ChildernProps = {
     children: React.ReactNode;
@@ -8,8 +9,10 @@ const MarketingLayout = ({ children }: ChildernProps) => {
     return (
         <div className="h-full bg-slate-100">
             {/* Navbar */}
+            <Navbar />
             <main className="pt-40 pb-20 bg-slate-100">{children}</main>
             {/* Footer */}
+            <Footer />
         </div>
     );
 };
